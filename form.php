b268e976c5a3646e03d6757b72e22a6c8dbae31f
LucasGawain <galvaol748@gmail.com>
2023-10-04 20:30:02 -0400

    0410232029

--- form.php
+++ form.php
@@ -5,62 +5,9 @@
 </head>
 <body>
     <h1>Gerenciamento de Funcionários</h1>
-    
-    <?php
-    // Inclua o arquivo de configuração do banco de dados
-    require_once('form.php');
-
-    // Função para exibir todos os funcionários
-    function listarFuncionarios($conn) {
-        $sql = "SELECT * FROM pt_funcionarios";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "<h2>Lista de Funcionários</h2>";
-            echo "<table border='1'>";
-            echo "<tr><th>ID</th><th>Nome</th><th>Cargo</th><th>Ações</th></tr>";
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['func_id'] . "</td>";
-                echo "<td>" . $row['func_nome'] . "</td>";
-                echo "<td>" . $row['func_cargo'] . "</td>";
-                echo "<td><a href='editar.php?id=" . $row['func_id'] . "'>Editar</a> | <a href='excluir.php?id=" . $row['func_id'] . "'>Excluir</a></td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-        } else {
-            echo "Nenhum funcionário encontrado.";
-        }
-    }
-
-    // Verifique se o formulário foi enviado para inserção
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Capturar os dados do formulário
-        $func_id = $_POST['func_id'];
-        $func_nome = $_POST['func_nome'];
-        $func_cargo = $_POST['func_cargo'];
-
-        // Inserir os dados na tabela
-        $sql = "INSERT INTO pt_funcionarios (func_id, func_nome, func_cargo) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("iss", $func_id, $func_nome, $func_cargo);
-
-        if ($stmt->execute()) {
-            echo "Dados inseridos com sucesso!";
-        } else {
-            echo "Erro ao inserir os dados: " . $stmt->error;
-        }
-        
-        // Fechar a conexão com o banco de dados
-        $stmt->close();
-    }
-
-    // Exibir a lista de funcionários
-    listarFuncionarios($conn);
-    ?>
 
     <h2>Adicionar Novo Funcionário</h2>
-    <form method="POST" action="">
+    <form method="POST" action="inserir.php">
         <label for="func_id">ID do Funcionário:</label>
         <input type="number" name="func_id" required><br><br>
 
@@ -71,6 +18,35 @@
         <input type="text" name="func_cargo"><br><br>
 
         <input type="submit" value="Adicionar">
+    </form>
+
+    <h2>Consultar Funcionário</h2>
+    <form method="GET" action="consultar.php">
+        <label for="consulta">Consultar por Nome:</label>
+        <input type="text" name="consulta" required><br><br>
+        <input type="submit" value="Consultar">
+    </form>
+
+    <h2>Atualizar Funcionário</h2>
+    <form method="POST" action="atualizar.php">
+        <label for="func_id">ID do Funcionário:</label>
+        <input type="number" name="func_id" required><br><br>
+
+        <label for="func_nome">Novo Nome:</label>
+        <input type="text" name="func_nome" required><br><br>
+
+        <label for="func_cargo">Novo Cargo:</label>
+        <input type="text" name="func_cargo"><br><br>
+
+        <input type="submit" value="Atualizar">
+    </form>
+
+    <h2>Excluir Funcionário</h2>
+    <form method="POST" action="excluir.php">
+        <label for="func_id">ID do Funcionário a ser Excluído:</label>
+        <input type="number" name="func_id" required><br><br>
+
+        <input type="submit" value="Excluir">
     </form>
 </body>
 </html>
---
+++ queryfunc.php
@@ -0,0 +1,31 @@
+<?php
+    // Inclua o arquivo de configuração do banco de dados
+    require_once('form.php');
+    $conn = mysqli_connect("localhost:3306","root","","ponto");
+
+    // Função para listar todos os funcionários
+    function listarFuncionarios($conn) {
+        $sql = "SELECT * FROM pt_funcionarios";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<h2>Lista de Funcionários</h2>";
+            echo "<table border='1'>";
+            echo "<tr><th>ID</th><th>Nome</th><th>Cargo</th><th>Ações</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['func_id'] . "</td>";
+                echo "<td>" . $row['func_nome'] . "</td>";
+                echo "<td>" . $row['func_cargo'] . "</td>";
+                echo "<td><a href='editar.php?id=" . $row['func_id'] . "'>Editar</a> | <a href='excluir.php?id=" . $row['func_id'] . "'>Excluir</a></td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "Nenhum funcionário encontrado.";
+        }
+    }
+
+    // Exibir a lista de funcionários
+    listarFuncionarios($conn);
+    ?>
