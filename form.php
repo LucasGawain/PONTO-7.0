81845c92f1c04553eb04e16ce8f2d9778e7434f4
LucasGawain <galvaol748@gmail.com>
2023-10-09 22:15:35 -0400

    0910232215

--- form.php
+++ form.php
@@ -1,22 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title >Gerenciamento de Funcionários</title>
+    <title>Gerenciamento de Funcionários</title>
     <style>
-        h1 {
-                text-align: center;
-            }
-            input[type=text] {
-                width: 20%;
-                box-sizing: border-box;
-                border: 2px solid #ccc;
-                border-radius: 4px;
-                font-size: 16px;
-                background-color: white;
-                background-position: 10px 10px; 
-                background-repeat: no-repeat;
-            }
-        
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1, h2 {
+            color: #333;
+            text-align: center;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type="submit"] {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
     </style>
 </head>
 <body>
@@ -25,13 +59,13 @@
     <h2>Adicionar Novo Funcionário</h2>
     <form method="POST" action="addfunc.php">
         <label for="func_id">ID do Funcionário:</label>
-        <input type="number" name="func_id" placeholder="Inserir ID" required><br><br>
+        <input type="number" name="func_id" required><br><br>
 
         <label for="func_nome">Nome do Funcionário:</label>
-        <input type="text" name="func_nome" placeholder="Inserir NOME" required><br><br>
+        <input type="text" name="func_nome" required><br><br>
 
         <label for="func_cargo">Cargo do Funcionário:</label>
-        <input type="text" name="func_cargo"  placeholder="Inserir CARGO"><br><br>
+        <input type="text" name="func_cargo"><br><br>
 
         <input type="submit" value="Adicionar">
     </form>
@@ -39,20 +73,20 @@
     <h2>Consultar Funcionário</h2>
     <form method="GET" action="queryfunc.php">
         <label for="consulta">Consultar por Nome:</label>
-        <input type="text" name="consulta" placeholder="Inserir ID"s required><br><br>
+        <input type="text" name="consulta" required><br><br>
         <input type="submit" value="Consultar">
     </form>
 
     <h2>Atualizar Funcionário</h2>
     <form method="POST" action="editfunc.php">
         <label for="func_id">ID do Funcionário:</label>
-        <input type="number" name="func_id" placeholder="Inserir ID" required><br><br>
+        <input type="number" name="func_id" required><br><br>
 
         <label for="func_nome">Novo Nome:</label>
-        <input type="text" name="func_nome" placeholder="Inserir NOME" required><br><br>
+        <input type="text" name="func_nome" required><br><br>
 
         <label for="func_cargo">Novo Cargo:</label>
-        <input type="text" name="func_cargo" placeholder="Inserir CARGO"><br><br>
+        <input type="text" name="func_cargo"><br><br>
 
         <input type="submit" value="Atualizar">
     </form>
@@ -60,7 +94,7 @@
     <h2>Excluir Funcionário</h2>
     <form method="POST" action="exclfunc.php">
         <label for="func_id">ID do Funcionário a ser Excluído:</label>
-        <input type="number" name="func_id" placeholder="Inserir ID" required><br><br>
+        <input type="number" name="func_id" required><br><br>
         <input type="submit" value="Excluir">
     </form>
 </body>
